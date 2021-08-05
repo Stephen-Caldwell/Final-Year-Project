130efe1158a5e91f387c47055596ed544f4423d8
Stephen-Caldwell <stephencaldwell137@gmail.com>
2021-08-05 19:25:52 +0100

    attempt to fix heroku deployment

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+app.secret_key  = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template("index.html")
 
